7c827fb4eb16424afb19b821e6120618826d1907
MichaelCrenshaw <57016022+MichaelCrenshaw@users.noreply.github.com>
2022-08-20 21:42:32 -0600

    Rebased conflicting code

---
+++ src/main/World.java
@@ -0,0 +1,39 @@
+package main;
+
+import util.LevelData;
+
+public class World {
+	
+	int[][] tiles;
+	int worldWidth, worldHeight;
+	
+	public World(int world_width, int world_height) {
+		this.worldWidth = world_width;
+		this.worldHeight = world_height;
+		
+		this.tiles = new int[worldWidth][worldHeight];
+	}
+	
+	public World(LevelData data) {
+		this.tiles = data.getWorld();
+		this.worldWidth = data.getWorldWidth();
+		this.worldHeight = data.getWorldHeight();
+	}
+
+	public int getWorldWidth() {
+		return worldWidth;
+	}
+
+	public int getWorldWeight() {
+		return worldHeight;
+	}
+	
+	public int getTile(int x, int y) {
+		return tiles[x][y];
+	}
+	
+	public void setTile(int type, int x, int y) {
+		this.tiles[x][y] = type;
+	}
+	
+}
